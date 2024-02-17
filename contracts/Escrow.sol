ee732852c180a73f78f7e36af373ce6e23cec1fd
Pedro Aquino <pedro.aquino479@gmail.com>
2024-02-17 03:09:15 +0100

    add earnest deposit and inspection

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -36,6 +36,8 @@ contract Escrow {
     mapping(uint256 => uint256) public escrowAmount;
     mapping(uint256 => address) public buyer;
     mapping(uint256 => bool) public inspectionPassed;
+    mapping(uint256 => mapping(address => bool)) public approval;
+
 
 
     constructor(address _nftAddress, address payable _seller, address _inspector, address _lender){
@@ -69,6 +71,11 @@ contract Escrow {
 
     function updateInspectionStatus(uint256 _nftID, bool _passed) public onlyInspector {
         inspectionPassed[_nftID] = _passed;
+    }
+
+    // approve sale
+    function approveSale(uint256 _nftID) public {
+        approval[_nftID][msg.sender] = true;
     }
 
 }
